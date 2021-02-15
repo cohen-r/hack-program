a835e06ce9ad9642b0b42f5af5321ec884ab5d79
Deren <de2356@columbia.edu>
2021-02-15 09:57:28 -0500

    fixed csv path (relative)

--- compliments/compliments.py
+++ compliments/compliments.py
@@ -5,20 +5,26 @@
 Setting up recieving a plant name and a complument based on mood
 
 """
-import numpy as np
+# import numpy as np
 import pandas as pd
 
+# GLOBALS: write path to csv *relative to current file (__file__)*
+# or you could just write the full path, the this relative mode will
+# work even if you install it not in development mode.
+CSVPATH = __file__.split(".py")[0] + ".csv"
+COMPLIMENTS = pd.read_csv(CSVPATH, dtype="string")
 
-compliments = pd.read_csv("compliments.csv", dtype = "string")
+# settings
 pd.set_option("display.max_colwidth", 10000)
+
 
 def compliment_me(mood):
     """
     The compliment_me function returns a compliment based on 1 of three moods: angry, sad, or happy
     """
 
-    sad_compliments = compliments["sad"]
-    angry_compliments = compliments["angry"]
+    sad_compliments = COMPLIMENTS["sad"]
+    angry_compliments = COMPLIMENTS["angry"]
 
     if mood == "sad":
         print("I'm sorry to hear that, " + sad_compliments.sample().to_string(index = False))
@@ -32,7 +38,7 @@ def plant_me():
     """
     Function that returns a randomly selected house plant
     """
-    plants = compliments["plant"]
+    plants = COMPLIMENTS["plant"]
     print("Check out this house plant to make you smile: " 
         + plants.sample().to_string(index = False))
 
